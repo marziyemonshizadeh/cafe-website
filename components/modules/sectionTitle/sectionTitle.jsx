d86b08cc1ced06b1835eb91ea703b7f9f51d52f0
marzieh <marziehmonshizade@yahoo.com>
2023-12-15 16:59:41 +0330

    create title modules

---
+++ components/modules/sectionTitle/sectionTitle.jsx
@@ -0,0 +1,10 @@
+export default function SectionTitle({ title }) {
+  return (
+    <div class="relative">
+      <div class="absolute left-1/2 -ml-0.5 w-0.5 h-28 bg-amber-500 mt-10"></div>
+      <p className="pt-40 text-amber-500 flex justify-center items-center font-mono text-lg font-bold">
+        {title}
+      </p>
+    </div>
+  );
+}
