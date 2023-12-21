ded4856cd634c1a9fced5bf1b02dfc249d7112a9
marzieh <marziehmonshizade@yahoo.com>
2023-12-22 02:13:43 +0330

    add logo to navbar

--- components/modules/navbar/navbar.jsx
+++ components/modules/navbar/navbar.jsx
@@ -4,10 +4,15 @@ export default function Navbar() {
   return (
     <div className="absolute z-50 w-full">
       <div className=" p-2 end-0 flex items-center md:justify-between justify-center py-2  text-black overflow-hidden">
-        <div className="text-2xl font-extrabold md:block hidden">
-          NEXT-COFFEE
+        <div className="text-2xl font-extrabold">
+          {/* NEXT-COFFEE */}
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/013/390/810/original/objects-about-coffee-for-logo-or-items-png.png"
+            alt="logo"
+            className="w-12 h-12"
+          />
         </div>
-        <div className="font-bold  grid md:grid-cols-6 grid-cols-3 md:gap-0 gap-6 ">
+        <div className="font-bold  grid grid-cols-6  md:gap-0 gap-6 ">
           <Link href="#" className="linksStyle">
             Home
           </Link>
