bcffe0d154d61974118f60c1aa4038278291c47f
marzieh <marziehmonshizade@yahoo.com>
2023-12-22 02:14:45 +0330

    add header and footer to _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Footer from "@/components/modules/footer/footer";
+import Header from "@/components/modules/header/header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
