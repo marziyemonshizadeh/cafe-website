a31abf5462b53cbf0cedf38e6b5589205515c772
marzieh <marziehmonshizade@yahoo.com>
2023-12-23 01:13:52 +0330

    create serviceItem component

---
+++ components/modules/ServiceItem/ServiceItem.jsx
@@ -0,0 +1,21 @@
+export default function ServiceItem() {
+  return (
+    <div className="flex md:flex-row flex-col items-center gap-8 justify-center m-3">
+      <img
+        src="https://techcrunch.com/wp-content/uploads/2020/03/GettyImages-1154103372.jpg"
+        alt="cafe"
+        className="h-48"
+      />
+      <div>
+        <p className="text-orange-950 text-lg font-medium py-2">
+          Fastest Door Delivery
+        </p>
+        <div>
+          Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et
+          loremdolor sed est sit invidunt, dolore tempor diam ipsum takima erat
+          tempor
+        </div>
+      </div>
+    </div>
+  );
+}
