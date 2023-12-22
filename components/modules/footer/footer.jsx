38daa7c01c2afb1c1b37aafe3ea72f890bab2805
marzieh <marziehmonshizade@yahoo.com>
2023-12-23 01:12:13 +0330

    coding footer icons

--- components/modules/footer/footer.jsx
+++ components/modules/footer/footer.jsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTelegramPlane,
+  FaWhatsapp,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
     <div
@@ -8,44 +17,57 @@ export default function Footer() {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div>
+      <section>
         <h2 className="font-mono text-2xl font-bold">GET IN TOUCH</h2>
         <div>123 Street, New York, USA</div>
         <div>+012 345 67890</div>
         <div>info@example.com</div>
-      </div>
-      <div>
+      </section>
+      <section>
         <h2 className="font-mono text-2xl font-bold">FOLLOW US</h2>
         <div>
           Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum
         </div>
-        <div>social media</div>
-      </div>
-      <div>
+        <div className="flex flex-row gap-4 my-3">
+          <Link href="#">
+            <FaTelegramPlane className="hover:text-amber-500 text-xl" />
+          </Link>
+          <Link href="#">
+            <FaInstagram className="hover:text-amber-500  text-xl" />
+          </Link>
+          <Link href="#">
+            <FaFacebookF className="hover:text-amber-500  text-xl" />
+          </Link>
+          <Link href="#">
+            <FaWhatsapp className="hover:text-amber-500  text-xl" />
+          </Link>
+          <Link href="#">
+            <FaLinkedinIn className="hover:text-amber-500  text-xl" />
+          </Link>
+        </div>
+      </section>
+      <section>
         <h2 className="font-mono text-2xl font-bold">OPEN HOURS</h2>
         <div>MONDAY - FRIDAY</div>
         <div>8.00 AM - 8.00 PM</div>
         <div>SATURDAY - SUNDAY</div>
         <div>2.00 PM - 8.00 PM</div>
-      </div>
-      <div>
+      </section>
+      <section>
         <h2 className="font-mono text-2xl font-bold">NEWSLETTER</h2>
         <div>
           Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum
         </div>
         <input
           type="search"
-          className="p-2 outline-none border-none leading-none text-slate-700 bg-gray-50 focus:ring-white focus:border-amber-700 "
+          className="p-1 outline-none border-none  text-slate-700 bg-gray-50 focus:ring-white focus:border-amber-700 "
           placeholder="Your Email"
           required
         />
-        <button
-          type="submit"
-          className="p-3 text-black leading-none bg-amber-400"
-        >
+        <button type="submit" className="p-1 text-black  bg-amber-400">
           Sign Up
         </button>
-      </div>
+      </section>
     </div>
   );
 }
